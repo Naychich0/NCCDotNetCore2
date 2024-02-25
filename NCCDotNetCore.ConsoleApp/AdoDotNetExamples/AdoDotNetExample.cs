3a5edab4465b11cc266f096e44da095ab87369f5
NayChiCho <naychicho.dev@gmail.com>
2024-02-25 22:12:52 +0630

    Add project files.

---
+++ NCCDotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCDotNetCore.ConsoleApp.AdoDotNetExamples
+{
+    //public
+    //private
+    //protected
+    //internal
+    public class AdoDotNetExample
+    {
+        
+    }
+}
